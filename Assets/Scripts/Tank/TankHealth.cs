2e8ac89dbc8d6d8d391e1367451926599fe45c59
Kahssie <73805707+Kahssie@users.noreply.github.com>
2022-07-07 01:31:51 +0800

    (1) NO MORE BAD DRIVING ALLOWED
    improvement 1 complete: audio and HP loss on terrain collision

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -3,25 +3,30 @@ using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-    public float m_StartingHealth = 100f;          
-    public Slider m_Slider;                        
-    public Image m_FillImage;                      
-    public Color m_FullHealthColor = Color.green;  
-    public Color m_ZeroHealthColor = Color.red;    
+    public float m_StartingHealth = 100f;
+    public Slider m_Slider;
+    public Image m_FillImage;
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
     public GameObject m_ExplosionPrefab;
-    
-    private AudioSource m_ExplosionAudio;          
-    private ParticleSystem m_ExplosionParticles;   
-    private float m_CurrentHealth;  
-    private bool m_Dead;            
+
+    private AudioSource m_ExplosionAudio;
+    public AudioSource audioSource;
+    public AudioClip m_BonkAudioClip;
+
+    private ParticleSystem m_ExplosionParticles;
+    private float m_CurrentHealth;
+    private bool m_Dead;
+    private Rigidbody m_RigidBody;
 
 
     private void Awake()
     {
         m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
         m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
-
         m_ExplosionParticles.gameObject.SetActive(false);
+        m_RigidBody = gameObject.GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
 
@@ -29,8 +34,28 @@ public class TankHealth : MonoBehaviour
     {
         m_CurrentHealth = m_StartingHealth;
         m_Dead = false;
-
         SetHealthUI();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // player decides to drive into non-tank objects
+        if (collision.gameObject.tag != "Player")
+        {
+            //Debug.Log("ram occurred");
+            var ramSpeed = collision.relativeVelocity.magnitude;
+            if (ramSpeed > 0.005)
+            {
+                // die for going high speed into walls
+                Debug.Log("WARNING PLAYER BOOM WALL");
+                m_RigidBody.GetComponent<TankHealth>().TakeDamage(100);
+            }
+            // lose some hp for small bumps
+            Debug.Log("player bonk wall");
+
+            audioSource.PlayOneShot(m_BonkAudioClip, 1.0F);
+            m_RigidBody.GetComponent<TankHealth>().TakeDamage(15);
+
+        }
     }
 
     public void TakeDamage(float amount)
@@ -39,7 +64,10 @@ public class TankHealth : MonoBehaviour
         m_CurrentHealth -= amount;
 
         SetHealthUI();
-        if (m_CurrentHealth <= 0f && !m_Dead) OnDeath();
+        if (m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
     }
 
 
